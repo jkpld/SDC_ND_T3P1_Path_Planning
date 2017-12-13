b6a3042b09b08408ca4594293b9d35426d1a7c11
James Kapaldo <james.kapaldo@gmail.com>
2017-12-13 15:33:53 -0500

    Add in code for several of the methods

--- src/TrajectoryGenerator.h
+++ src/TrajectoryGenerator.h
@@ -10,11 +10,13 @@ sited as [1].)
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 #include "Eigen-3.3/Eigen/Core"
 #include "polynomial.h"
 #include "roots.h"
 #include "helpers.h"
 #include "Vehicle.h"
+
 
 using namespace Eigen;
 using namespace std;
@@ -22,47 +24,47 @@ using namespace std;
 class TrajectoryGenerator {
 public:
   /* Weights for cost function, as defined in the paper [1]*/
-  double const kj; // weight for the jerk cost
-  double const kt; // weight fot the time-to-complete cost
-  double const ks; // weight for the difference in s-coordinate cost
-  double const ksd; // weight for the difference in s-dot-coordinate cost
-  double const kd; // weight for the difference in d-coordinate cost
+  double kj; // weight for the jerk cost
+  double kt; // weight fot the time-to-complete cost
+  double ks; // weight for the difference in s-coordinate cost
+  double ksd; // weight for the difference in s-dot-coordinate cost
+  double kd; // weight for the difference in d-coordinate cost
 
-  double const k_lat; // weight for lateral trajectories
-  double const k_lon; // weight for longitudinal trajectories
+  double k_lat; // weight for lateral trajectories
+  double k_lon; // weight for longitudinal trajectories
 
   /* Safty_margin - This will be used to ensure the cars do not collide
   or come to close to each other. */
-  Vector2d const Safty_Margin;
+  Vector2d Safty_Margin;
 
   /* Parameters for constant distance and constant time law. */
-  double const D0; // static distance required between cars at speed 0
-  double const tau; // time distance between cars
+  double D0; // static distance required between cars at speed 0
+  double tau; // time distance between cars
 
   /* Speed, acceleration, and jerk maximum values.
     These should be the highest possible values that the vehicle can function
     at; however, here, they will simply be the maximum values allowed by the
     project.
   */
-  double const Max_Speed;
-  double const Max_Accel;
-  double const Max_Jerk;
+  double Max_Speed;
+  double Max_Accel;
+  double Max_Jerk;
 
   /* Information on the road, that really shouldn't be here probably. */
-  double const Min_D;
-  double const Max_D;
-  double const Lane_Width;
+  double Min_D;
+  double Max_D;
+  double Lane_Width;
 
   /* Time horizons */
-  double const Time_Horizon; // used in checking for collisions
-  double const Reactive_Layer_Time_Horizon;
+  double Time_Horizon; // used in checking for collisions
+  double Reactive_Layer_Time_Horizon;
 
 private:
   /* Properties controling the search values for the new trajectories */
-  VectorXd const T_; // times used in searching for new trajectories
-  VectorXd const ds_; // offset longitudinal distances
-  VectorXd const dsd_; // offset speeds
-  VectorXd const d_; // offset lateral distances
+  ArrayXd T_; // times used in searching for new trajectories
+  ArrayXd ds_; // offset longitudinal distances
+  ArrayXd dsd_; // offset speeds
+  ArrayXd d_; // offset lateral distances
 
 public:
   // Constructor with default values
@@ -85,21 +87,21 @@ public:
     Lane_Width(1),
     Time_Horizon(3),
     Reactive_Layer_Time_Horizon(1),
-    T_(VectorXd::LinSpaced(3,1,3)),
-    ds_(VectorXd::LinSpaced(11,-5,5)),
-    dsd_(VectorXd::LinSpaced(5,-2,2)),
-    d_(VectorXd::LinSpaced(3,-1,1)) {};
+    T_(ArrayXd::LinSpaced(3,1,3)),
+    ds_(ArrayXd::LinSpaced(11,-5,5)),
+    dsd_(ArrayXd::LinSpaced(5,-2,2)),
+    d_(ArrayXd::LinSpaced(3,-1,1)) {};
 
   /* Generate the best trajectory for ego using the SearchMode while preventing
   collisions with the cars. If no trajectory is found, then false is returned;
   otherwise, true is returned.
   */
-  bool generate(Vehicle& ego, vector<Vehicle> const& cars, SearchMode const& SM) {
+  bool generate(Vehicle& ego, vector<Vehicle> const& cars, SearchMode const& SM) const {
     return generate_(ego, cars, SM, d_, dsd_);
   }
 
 private:
-  bool generate_(Vehicle& ego, vector<Vehicle> const& cars, SearchMode const& SM, VectorXd const& search_d, VectorXd const& search_sd) {
+  bool generate_(Vehicle& ego, vector<Vehicle> const& cars, SearchMode const& SM, ArrayXd const& search_d, ArrayXd const& search_sd) const {
 
     vector<Trajectory> trajs_d, trajs_s;
     vector<double> costs_d, costs_s;
@@ -115,15 +117,15 @@ private:
 
     // Search for the longitudinal trajectories
     for (int i=0; i<SM.activeModes.size(); ++i) {
-      string mode = SM.activeModes[i].mode;
+      Mode mode = SM.activeModes[i].mode;
 
-      if (mode.compare("following") ) {
+      if (mode == Mode::FOLLOWING ) {
         following(state0, SM.activeModes[i].state[0], trajs_s, costs_s);
-      } else if (mode.compare("merging") ) {
+      } else if (mode == Mode::MERGING ) {
         merging(state0, SM.activeModes[i].state[0], SM.activeModes[i].state[1], trajs_s, costs_s);
-      } else if (mode.compare("stopping") ) {
+      } else if (mode == Mode::STOPPING ) {
         stopping(state0, SM.activeModes[i].number, trajs_s, costs_s);
-      } else if (mode.compare("velocity_keeping") ) {
+      } else if (mode == Mode::VELOCITY_KEEPING ) {
         velocity_keeping(state0, SM.activeModes[i].number, search_sd, trajs_s, costs_s);
       }
     }
@@ -140,8 +142,8 @@ private:
      ascending order. The linear indices are i*costs_d.size() + j.
     */
     auto idx = sort_outer_sum(costs_d, costs_s);
-    int const Nd = costs_d.size(); // number of d-trajectories
-    int const Ns = costs_s.size(); // number of s-trajectories
+    int Nd = costs_d.size(); // number of d-trajectories
+    int Ns = costs_s.size(); // number of s-trajectories
 
     // If there are no cars, then simply use the lowest cost trajectory
     // combination
@@ -157,8 +159,8 @@ private:
       // with the cars, and use the first combination that does not have any
       // collisions.
 
-      int const Nt = ceil(Time_Horizon * 0.2) + 1;
-      int const Nc = cars.size();
+      int Nt = ceil(Time_Horizon * 0.2) + 1;
+      int Nc = cars.size();
 
       // Construct the car trajectories for collision detection and the minimum
       // distance between ego and each car at which there cannot be a collision
@@ -168,14 +170,15 @@ private:
 
       for (int ci=0; ci<Nc; ++ci) {
         // get the car locations
-        car_traj.block<Nt,2>(ci*Nt,0) = cars[ci].location_Eig(t);
+        car_traj.block(Nt,2,ci*Nt,0) = cars[ci].location_Eig(t);
 
         // half diagonal of the car
-        min_collision_free_dist(ci) = ((Vector2d()<<cars[ci].Length,cars[ci].Width).finished()/2 + Safty_Margin).norm();
+        min_collision_free_dist(ci) = (cars[ci].get_size()/2 + Safty_Margin).norm();
       }
-      min_collision_free_dist += ((Vector2d()<<ego.Length,ego.Width).finished()/2 + Safty_Margin).norm(); // add the half diagonal of ego
+      min_collision_free_dist += (ego.get_size()/2 + Safty_Margin).norm(); // add the half diagonal of ego
       min_collision_free_dist *= min_collision_free_dist; // square it so later we don't have to take square roots of the distances between the cars.
 
+      bool collide = false;
       for (int ds_i=0; ds_i<Nd*Ns; ++ds_i) {
         // Get the indices of the current trajectory combination
         int di,si;
@@ -185,7 +188,7 @@ private:
         ego.set_trajectory({trajs_d[di], trajs_s[si]});
 
         // Detect collisions
-        bool collide = detect_collision(ego, cars, car_traj, t, min_collision_free_dist);
+        collide = detect_collision(ego, cars, car_traj, t, min_collision_free_dist);
         if (!collide) break;
       }
 
@@ -200,24 +203,24 @@ private:
     return true;
   }
 
-  bool reactive(Vehicle& ego, vector<Vehicle> const& cars) {
+  bool reactive(Vehicle& ego, vector<Vehicle> const& cars) const {
     return true;
   }
 
-  bool detect_collision(Vehicle const& ego, vector<Vehicle> const& cars, MatrixXd car_traj, VectorXd t, VectorXd min_collision_free_dist){
+  bool detect_collision(Vehicle& ego, vector<Vehicle> const& cars, MatrixXd car_traj, VectorXd t, VectorXd min_collision_free_dist)const {
 
     // NOTE: I am using just axis aligned bounding box instead of the exact
     // bounding box. Switch to object aligned bounding box by chaning this flag
     // to true;
     bool OA_DETECTION = false;
 
-    int const Nc = cars.size();
-    int const Nt = t.size();
+    int Nc = cars.size();
+    int Nt = t.size();
 
     MatrixXd ego_traj = ego.location_Eig(t);
     MatrixXd dist(Nt,Nc);
     for (int ci=0; ci<Nc; ++ci) {
-      dist.col(ci) = (car_traj.block<Nt,2>(ci*Nt,0) - ego_traj).rowwise().squaredNorm() / min_collision_free_dist(ci);
+      dist.col(ci) = (car_traj.block(Nt,2,ci*Nt,0) - ego_traj).rowwise().squaredNorm() / min_collision_free_dist(ci);
     }
 
     // Find all the points (car/time combinations) that we must check for a collision.
@@ -233,68 +236,157 @@ private:
       // the indices in ascending order of the distance, and start going through
       // the points from the closest. This should help reduce work when there is
       // a collision.
-      sort(idx.data(), idx.data()+idx.size(), [&dist](size_t i1, size_t i2) {return dist[i1] < dist[i2];});
+      sort(idx.data(), idx.data()+idx.size(), [&dist](size_t i1, size_t i2) {return dist(i1) < dist(i2);});
 
       for (int ct_i=0; ct_i<idx.size(); ++ct_i) {
         int ti,ci;
         ind2sub(Nt,idx[ct_i],ti,ci);
-        rec1 = ego.bounding_box(t(ti), ego_traj.row(ti), Safty_Margin);
-        rec2 = ego.bounding_box(t(ti), car_traj.row(ci*Nt+ti), Safty_Margin);
-        if (rec1.collide(rec2, OA_DETECTION)) return true;
+        Rectangle rec1 = ego.bounding_box(t(ti), ego_traj.row(ti), Safty_Margin);
+        Rectangle rec2 = ego.bounding_box(t(ti), car_traj.row(ci*Nt+ti), Safty_Margin);
+        if (rec1.overlap(rec2, OA_DETECTION)) return true;
       }
     }
     return false;
   }
 
-  void lateral(State const& state0, double const& goal_lane, VectorXd const& search_d, vector<Trajectory>& trajs, vector<double>& costs) {
-    // NOTE! Multiply the final costs by k_lat
+  void lateral(State const& state0, double const& goal_lane, ArrayXd const& search_d, vector<Trajectory>& trajs, vector<double>& costs) const {
+
+    // Initial lateral state.
+    Vector3d d0 = state0.y;
+
+    // The goal state.
+    double d_goal = (goal_lane - 0.5)*Lane_Width; // Goal d-coordinate.
+    Vector3d dg;
+    dg << d_goal, 0, 0;
+
+    // The search d values.
+    ArrayXd d = search_d + d_goal;
+
+    for (int di =0 ; di<search_d.size(); ++di) {
+      // Do not search for any trajectory that takes us off the road.
+      if (d(di)<Min_D || d(di)>Max_D) continue;
+
+      for (int ti = 0; ti<T_.size(); ++ti) {
+        Vector3d d1;
+        d1 << d(di), 0, 0;
+
+        auto coefs = JMT(d0, d1, T_(ti));
+        if (!compute_validity(coefs, T_(ti))) continue;
+
+        costs.push_back(compute_cost(coefs,T_(ti), d1-dg, Trajectory(), Mode::LATERAL));
+        trajs.push_back(Trajectory(coefs, d1, T_(ti)));
+      }
+    }
   }
 
-  void following(State const& state0, State const& state_lv, vector<Trajectory>& trajs, vector<double>& costs) {
+  void following(State const& state0, State const& state_lv, vector<Trajectory>& trajs, vector<double>& costs) const {
+    // Get the goal trajectory using the constant distance constant time law
+    // Trajectory in x dimension.
+    Vector3d slv = state_lv.x;
+    slv(2) *= 0.5;
+
+    // Speed in x dimension.
+    Vector3d slv_d = polyder(slv, true);
+
+    // Offset
+    Vector3d d0;
+    d0 << D0, 0, 0;
+
+    Trajectory target(slv - (d0 + tau*slv_d));
+
+    // Compute the trajectories/costs
+    following_merging_stoping(state0, target, Mode::FOLLOWING, trajs, costs);
+  }
+
+  void merging(State const& state0, State const& state_a, State const& state_b, vector<Trajectory>& trajs, vector<double>& costs) const {
+    Vector3d st = (state_a.x + state_b.x)/2;
+    st(2) *= 0.5;
+    Trajectory target(st);
+
+    // Compute the trajectories/costs
+    following_merging_stoping(state0, target, Mode::MERGING, trajs, costs);
+  }
+
+  void stopping(State const& state0, double s_end, vector<Trajectory>& trajs, vector<double>& costs) const {
+    Vector3d st;
+    st << s_end, 0, 0;
+    Trajectory target(st);
+
+    // Compute the trajectories/costs
+    following_merging_stoping(state0, target, Mode::STOPPING, trajs, costs);
+  }
+
+  void velocity_keeping(State const& state0, double sd, ArrayXd const& search_sd, vector<Trajectory>& trajs, vector<double>& costs) const {
     // NOTE! Multiply the final costs by k_lon
   }
 
-  void merging(State const& state0, State const& state_a, State const& state_b, vector<Trajectory>& trajs, vector<double>& costs) {
-    // NOTE! Multiply the final costs by k_lon
-  }
-
-  void stopping(State const& state0, double s_end, vector<Trajectory>& trajs, vector<double>& costs) {
-    // NOTE! Multiply the final costs by k_lon
-  }
-
-  void velocity_keeping(State const& state0, double sd, VectorXd const& search_sd, vector<Trajectory>& trajs, vector<double>& costs) {
-    // NOTE! Multiply the final costs by k_lon
-  }
-
-  VectorXd JMT(Vector3d s0, Vector3d s1, double T) {
+  VectorXd JMT(Vector3d s0, Vector3d s1, double T) const {
     return VectorXd::Zero(6);
   }
 
-  VectorXd JMT_vel_keep(Vector3d s0, Vector3d s1, double T) {
+  VectorXd JMT_vel_keep(Vector3d s0, Vector3d s1, double T) const {
     return VectorXd::Zero(6);
   }
 
-  void following_merging_stoping(State const& state0, Trajectory const& target, string const& type, vector<Trajectory>& trajs, vector<double>& costs) {
+  void following_merging_stoping(State const& state0, Trajectory const& target, Mode const& mode, vector<Trajectory>& trajs, vector<double>& costs) const {
+    // Initial state
+    Vector3d s0 = state0.x;
 
+    for (int ti = 0; ti<T_.size(); ++ti) {
+      // Target state at t.
+      Vector3d s1t = target.state_at(T_(ti));
+
+      for (int dsi = 0 ; dsi<ds_.size(); ++dsi) {
+        // add in s-coordinate offset
+        Vector3d s1 = s1t;
+        s1(0) += ds_(dsi);
+
+        auto coefs = JMT(s0, s1, T_(ti));
+        if (!compute_validity(coefs, T_(ti))) continue;
+
+        costs.push_back(compute_cost(coefs,T_(ti), s1, target, mode));
+        trajs.push_back(Trajectory(coefs, s1, T_(ti)));
+      }
+    }
   }
 
-  double following_merging_stopping_cost(double T, State const& s1, Trajectory const& target) {
-    return 0.0;
+  double compute_cost(VectorXd const& coefs, double T, Vector3d const& s1, Trajectory const& target, Mode mode) const {
+    // Jerk cost, Cj
+    VectorXd p3 = polyder(polyder(polyder(coefs))); // jerk
+    VectorXd int_p32 = polyint(polymult(p3,p3)); // integral of jerk^2
+    double Cj = kj * polyval(int_p32,T); // jerk cost
+
+    // Time cost
+    double Ct = kt * T;
+
+    VectorXd targ = target.ppeval(T);
+
+    // Mode cost
+    double Cd;
+    switch (mode) {
+      case Mode::FOLLOWING:
+      case Mode::MERGING:
+      case Mode::STOPPING:
+        Cd = ks * pow(s1(0) - targ(0),2);
+        break;
+      case Mode::VELOCITY_KEEPING:
+        Cd = ksd * pow(s1(1) - targ(1),2);
+        break;
+      case Mode::LATERAL:
+        Cd = kd * s1(0)*s1(0);
+        break;
+    }
+
+    // Total cost
+    double cost = Cj + Ct + Cd;
+
+    // Multiply by lateral, longitudinal weight
+    cost *= (mode == Mode::LATERAL) ? k_lat : k_lon;
+
+    return cost;
   }
 
-  double velocity_keeping_cost(State const& s1, double target) {
-    return 0.0;
-  }
-
-  double lateral_cost(State const& d1) {
-    return 0.0;
-  }
-
-  double compute_cost(VectorXd const& coefs, double T, State const& s1, Trajectory const& target, string type) {
-    return 0.0;
-  }
-
-  bool compute_validity(VectorXd const& coefs, double T) {
+  bool compute_validity(VectorXd const& coefs, double T) const {
     return true;
   }
 
